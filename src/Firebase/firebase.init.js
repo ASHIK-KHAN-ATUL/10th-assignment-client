b691629b86fec9968f6995d61f4d0fea6c110269
ASHIK-KHAN-ATUL <ashikkhan693693@gmail.com>
2024-12-10 20:08:57 +0600

    secure credential

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBdmkxgpamD8Ojf7_Q_RNJT7m-uoRu_HD0",
-  authDomain: "assignment-10th-9f956.firebaseapp.com",
-  projectId: "assignment-10th-9f956",
-  storageBucket: "assignment-10th-9f956.firebasestorage.app",
-  messagingSenderId: "191177230592",
-  appId: "1:191177230592:web:d92373ca7d75068fe24be5"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
